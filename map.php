da73b731c133b53836be98361b5527bdc200832f
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-04-01 16:21:45 +0100

    Map is still not working correctly

--- map.php
+++ map.php
@@ -1,9 +1,6 @@
 <?php
 $view = new stdClass();
-$view ->pageTitle = 'Map';
+$view->pageTitle = 'Map';
 $view->login_status = false;
 require_once("logincontroller.php");
-
-
-
-require_once ('Views/map.phtml');
+require('Views/map.phtml');
