4c95764de7c1e259df64ea8bcbc02f01e4c78a93
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-04-03 18:08:03 +0100

    SQL to JSON conversion
    Added a Charger Database conversion to JSON for ability to work with JavaScript

--- Models/ChargerDataSet.php
+++ Models/ChargerDataSet.php
@@ -161,4 +161,22 @@ class ChargerDataSet
         $statement = $this->_dbHandle->prepare($sqlQuery);
         $statement->execute();
     }
+    public function chargerToJson()
+    {
+        $sqlQuery = 'SELECT * FROM charge_point';
+
+        $statement = $this->_dbHandle->prepare($sqlQuery);
+        $statement->execute();
+
+        $chargerData = [];
+        while ($row = $statement->fetch((PDO::FETCH_ASSOC)))
+        {
+            $chargerData[] = $row;
+        }
+        return json_encode($chargerData);
+
+
+
+
+    }
 }
--- map.php
+++ map.php
@@ -3,4 +3,8 @@ $view = new stdClass();
 $view->pageTitle = 'Map';
 $view->login_status = false;
 require_once("logincontroller.php");
+require_once ('Models/ChargerDataSet.php');
+$chargerDataSet = new ChargerDataSet();
+$view->chargerDataSet = $chargerDataSet->chargerToJson();
+
 require('Views/map.phtml');
